8d171c00d50ae359bf25cff706876e533c11a3d0
carlalrfranca <cleticia@student.42sp.org.br>
2023-07-18 22:09:30 -0300

    adds files

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # WebServ
+
+A proposta é criar um servidor HTTP usando Multiplexação de E/S [entrada e saída] em um loop de eventos.
+
+
+## Multiplexação
+O prpósito é transmitir simultaneamente dois ou mais sinais individuais, de forma simultanea por meio de um único canal de comunicação.
+
+## O Loop de Eventos
+
+
+## Sockets
+"É como uma tomada. Diferentes formatos e diferentes padrões de buracos. E somente plugs corretos se encaixam em determinadas tomadas.O conjunto de endereço IP e porta de saída, do seu navegador conectando em um site e o endereço IP e a porta de entrada do site - as quatro informações é o mínimo para definir um socket. Ou seja, é o plug e a tomada conectada.
+o seu 192.168.1.200:4987  e do site 142.250.219.206:443
+Esse é o conjunto de informações necessário que você precisa para formar uma conexão de rede entre dois processos que é o caminho onde os pacotes vão trafegar.
+Sockets é uma forma de comunicação entre processos."
+
+
+
+
+## Referências
+
---
+++ inc/Response.hpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Response.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cleticia <cleticia@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/18 18:00:34 by cleticia          #+#    #+#             */
+/*   Updated: 2023/07/18 22:05:30 by cleticia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _RESPONSE_HPP_
+#define _RESPONSE_HPP_
+
+#include <string>
+#include <iostream>
+
+//acho que ele puxa por chave e valor entao perguntar depois se vamos usar map mesmo
+
+
+class Server;
+class Request;
+
+
+class Response {
+
+    public:
+        Response(void);
+        ~Response(void);
+        Response(Request *req, Server *_server); //precisa desses dois
+        
+        
+        //getters and setters
+        size_t getIdClient()const;
+        const std::string& getHeader(const std::string& header) const;
+        
+        //response
+        void status();
+        void text();
+        void reset();
+        // ..to be continue
+    
+    
+    private:
+    
+};
+
+std::ostream& operator<<(std::ostream& out, Response const& rhs);
+
+#endif //_                                                                          */
