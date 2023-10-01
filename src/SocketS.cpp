dc8f4ad25f4732d4362656c3198ca8933c0daa22
Leticia <canumsang@gmail.com>
2023-10-01 14:25:57 -0300

    fixes order calling servername

--- src/SocketS.cpp
+++ src/SocketS.cpp
@@ -6,7 +6,7 @@
 /*   By: lfranca- <lfranca-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 17:46:53 by cleticia          #+#    #+#             */
-/*   Updated: 2023/10/01 13:41:13 by lfranca-         ###   ########.fr       */
+/*   Updated: 2023/10/01 14:23:25 by lfranca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,7 @@ SocketS::SocketS()
 }
 
 
-SocketS::~SocketS()
-{}
+SocketS::~SocketS(){}
 
 // getter e setter do locations
 
@@ -157,14 +156,11 @@ void SocketS::initServer()
         throw SocketSException("Socket Error: Failed to create socket!");
     }
 	//configura endereço do servidor e inicializa os campos da estrutura com 0
-    // struct sockaddr_in server_address = {0, AF_INET, htons(std::atoi(getPort().c_str())), INADDR_ANY};
-
     struct sockaddr_in server_address = {0};
     server_address.sin_family = AF_INET; //socket usará os ends. IPv4
     server_address.sin_port = htons(std::atoi(getPort().c_str())); //usa a função htons para converter8080 para a ordem de bytes da rede e atribui a sin_port
     server_address.sin_addr.s_addr = INADDR_ANY; //especifica o end.IP que o socket do server será vinculado
-	
-	//chamada para o bind - vincula o socket ao endereço e porta, 0 -1 tem haver com a falha na chamada do bind
+    //chamada para o bind - vincula o socket ao endereço e porta, 0 -1 tem haver com a falha na chamada do bind
     if(bind(getWebServSocket(),(struct sockaddr*)&server_address, sizeof(server_address)) == -1)
 	{
         close(getWebServSocket());
@@ -176,14 +172,18 @@ void SocketS::initServer()
         close(getWebServSocket());
         throw SocketSException("Socket Error: Listen failed!");
 	}
-	for(std::vector<std::string>::const_iterator it = _serverName.begin(); it != _serverName.end(); ++it)
-        const std::string& serverName = *it;
-    if(!_serverName.empty())
-    {
-        const std::string& serverName = _serverName[0];
-        addAliasToHostsFile(serverName);
-    }
+	std::string serverName;
+	if(!_serverName.empty())
+	{
+		for(std::vector<std::string>::const_iterator it = _serverName.begin(); it != _serverName.end(); ++it)
+			serverName = *it;
+		addAliasToHostsFile(serverName);
+	}
 }
+
+
+
+
 
 // int SocketS::bindSocketListenConnections(){
 // 
@@ -226,7 +226,9 @@ void SocketS::initServer()
     }
     
     */
-
+    
+    
+    
 
     // aqui vão ser chamados os métodos (que serão criados ainda)
     // pra parseamento do arquivo de configuração
@@ -244,5 +246,4 @@ void SocketS::initServer()
     server_address.sin_port = htons(8080); 
     server_address.sin_addr.s_addr = INADDR_ANY;
     */
-    
  
